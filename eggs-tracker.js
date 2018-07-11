6e8b04d9f14a34841223afaeec24df4a5ae921fb
Radu Cosma <radu.cosma@lola.tech>
2018-07-11 18:03:12 +0300

    added support for 7k eggs

--- eggs-tracker.js
+++ eggs-tracker.js
@@ -23,12 +23,14 @@ var initialEggData = JSON.stringify({
 	initialTotals = {
 		'two' : -1,
 		'five' : -1, 
+		'seven': -1,
 		'ten' : -1
 	},
 	
 	totals = {
 		'two' : 0,
 		'five' : 0, 
+		'seven': 0,
 		'ten' : 0
 	},
 		
@@ -46,9 +48,27 @@ var initialEggData = JSON.stringify({
 	
 	fiveColumnPlaceholder = '%%5k%%',
 	
+	sevenColumnPlaceholder = "%%7k%%",
+	
 	tenColumnPlaceholder = '%%10k%%',
 	
-	rowTemplate = '<tr id="%%tr_id%%" class="%%tr_class%%"><td class="narrow total"><div id="%%total_id%%">%%total%%</div><div class="date">%%since%%</div></td> <td type="two">%%2k%%</td> <td type="five">%%5k%%</td> <td type="ten">%%10k%%</td> </tr>',
+	eggsSummaryRowTemplate = 
+		'<tr class="slim-border">' +
+			'<td colspan="4">' +
+				'<div id="%%total_id%%">' +
+					'<label class="total">%%total%%</label>' +
+					'<label class="date">%%since%%</label>' +
+				'</div>' +
+			'</td>' +
+		'</tr>',
+		
+	eggsCountRowTemplate = 
+		'<tr id="%%tr_id%%" class="%%tr_class%%">' + 
+			'<td type="two">%%2k%%</td>' +
+			'<td type="five">%%5k%%</td>' +
+			'<td type="seven">%%7k%%</td>' +
+			'<td type="ten">%%10k%%</td>' +
+		'</tr>',
 	
 	valuePlaceholder = '%%value%%',
 	
@@ -58,9 +78,23 @@ var initialEggData = JSON.stringify({
 	
 	percentageIdPlaceHolder = '%%percentage_id%%',
 	
-	inactiveColumnTemplate = '<div> <input id="%%input_id%%" value="%%value%%" disabled/> </div> <div id="%%percentage_id%%">%%percentage%%%</div>',
+	inactiveColumnTemplate = 
+		'<div>' +
+			'<input id="%%input_id%%" value="%%value%%" disabled/>' +
+		'</div>' +
+		'<div id="%%percentage_id%%">' +
+			'%%percentage%%%' +
+		'</div>',
 	
-	activeColumnTemplate = '<div> <button class="minus">-</button> <input id="%%input_id%%" value="%%value%%" disabled/> <button class="plus">+</button> </div> <div id="%%percentage_id%%">%%percentage%%%</div>',
+	activeColumnTemplate = 
+		'<div>' +
+			'<button class="minus">-</button>' +
+			'<input id="%%input_id%%" value="%%value%%" disabled/>' +
+			'<button class="plus">+</button>' +
+		'</div>' + 
+		'<div id="%%percentage_id%%">' +
+			'%%percentage%%%' +
+		'</div>',
 	
 	eggData,
 	
@@ -68,36 +102,42 @@ var initialEggData = JSON.stringify({
 		var table = $('#table');
 			
 		for (var i = 0; i < eggData.lines.length; i++) {
-			var row = rowTemplate, 
+			var eggsSummaryRow = eggsSummaryRowTemplate,
+				eggsCountRow = eggsCountRowTemplate,
 				line = eggData.lines[i],
 				columns = {};
 				
-			$.each([ 'two', 'five', 'ten' ], function( index, value ) {
-  				if (typeof line[value] === 'undefined') {
+			$.each([ 'two', 'five', 'seven', 'ten' ], function( index, value ) {
+				if (line.history === true && typeof line[value] === 'undefined') {
 					columns[value] = '';
 				} else {
 					if (initialTotals[value] === -1) {
-						initialTotals[value] = eggData.lines[i -1].total;
+						initialTotals[value] = eggData.lines[i-1].total;
 					}
-					totals[value] += line[value];
 					if (line.history === true) {
 						columns[value] = createColumn(inactiveColumnTemplate, value, i);
 					} else {
+						if (typeof line[value] === 'undefined') {
+							eggData.lines[i][value] = 0;
+						}
 						columns[value] = createColumn(activeColumnTemplate, value, i);
 					}
+					totals[value] += line[value];
 				}
 			});
-				
-			row = row.replace(trIdPlaceholder, i);
-			row = row.replace(trClassPlaceholder, line.history ===true ? 'history' : '');
-			row = row.replace(totalIdPlaceholder, i + '-total');
-			row = row.replace(totalPlaceholder, line.total);
-			row = row.replace(sincePlaceholder, typeof line.since === 'undefined' ? '' : line.since);
-			row = row.replace(twoColumnPlaceholder, columns['two']);
-			row = row.replace(fiveColumnPlaceholder, columns['five']);
-			row = row.replace(tenColumnPlaceholder, columns['ten']);
 			
-			table.append(row);
+			eggsSummaryRow = eggsSummaryRow.replace(totalIdPlaceholder, i + '-total');
+			eggsSummaryRow = eggsSummaryRow.replace(totalPlaceholder, line.total + ' eggs');
+			eggsSummaryRow = eggsSummaryRow.replace(sincePlaceholder, typeof line.since === 'undefined' ? '' : 'started on ' + line.since);	
+			eggsCountRow = eggsCountRow.replace(trIdPlaceholder, i);
+			eggsCountRow = eggsCountRow.replace(trClassPlaceholder, line.history === true ? 'history' : '');
+			eggsCountRow = eggsCountRow.replace(twoColumnPlaceholder, columns['two']);
+			eggsCountRow = eggsCountRow.replace(fiveColumnPlaceholder, columns['five']);
+			eggsCountRow = eggsCountRow.replace(sevenColumnPlaceholder, columns['seven']);
+			eggsCountRow = eggsCountRow.replace(tenColumnPlaceholder, columns['ten']);
+			
+			table.append(eggsSummaryRow);
+			table.append(eggsCountRow);
 		}
 		
 		table.append(createSummaryRow(eggData.lines[eggData.lines.length - 1].total, totals));
@@ -110,35 +150,40 @@ var initialEggData = JSON.stringify({
 			value;
 		
 		switch(type) {
-    		case 'two':
-        		value = eggData.lines[index].two;
-        		break;
-    		case 'five':
-        		value = eggData.lines[index].five;
-        		break;
-        	case 'ten':
-        		value = eggData.lines[index].ten;
-        		break;
-    		default:
-        		value = 0;
+			case 'two':
+				value = eggData.lines[index].two;
+				break;
+			case 'five':
+				value = eggData.lines[index].five;
+				break;
+			case 'seven':
+				value = eggData.lines[index].seven;
+				break;
+			case 'ten':
+				value = eggData.lines[index].ten;
+				break;
+			default:
+				value = 0;
 		}
 		
 		return fillColumn(columnTemplate, index + '-' + type + '-value', value, index + '-' + type + '-percentage', value === 0 ? 0 : parseFloat(value * 100 / (total - prevTotal)).toFixed(2));
 	},
 	
 	createSummaryRow = function( total, totals ) {
-		var row = rowTemplate;
+		var eggsSummaryRow = eggsSummaryRowTemplate,
+			eggsCountRow = eggsCountRowTemplate,
 		
-		row = row.replace(trIdPlaceholder, '');
-		row = row.replace(trClassPlaceholder, '');
-		row = row.replace(totalIdPlaceholder, '');
-		row = row.replace(totalPlaceholder, 'Summary');
-		row = row.replace(sincePlaceholder, '');
-		row = row.replace(twoColumnPlaceholder, fillColumn(inactiveColumnTemplate, 'summary-two-value', totals['two'], 'summary-two-percentage', parseFloat(totals['two'] * 100 / (total - initialTotals['two'])).toFixed(2)));
-		row = row.replace(fiveColumnPlaceholder, fillColumn(inactiveColumnTemplate, 'summary-five-value', totals['five'], 'summary-five-percentage', parseFloat(totals['five'] * 100 / (total - initialTotals['five'])).toFixed(2)));
-		row = row.replace(tenColumnPlaceholder, fillColumn(inactiveColumnTemplate, 'summary-ten-value', totals['ten'], 'summary-ten-percentage', parseFloat(totals['ten'] * 100 / (total - initialTotals['ten'])).toFixed(2)));
+		eggsSummaryRow = eggsSummaryRow.replace(totalIdPlaceholder, '');
+		eggsSummaryRow = eggsSummaryRow.replace(totalPlaceholder, 'Summary');
+		eggsSummaryRow = eggsSummaryRow.replace(sincePlaceholder, '');
+		eggsCountRow = eggsCountRow.replace(trIdPlaceholder, '');
+		eggsCountRow = eggsCountRow.replace(trClassPlaceholder, 'summary-row');
+		eggsCountRow = eggsCountRow.replace(twoColumnPlaceholder, fillColumn(inactiveColumnTemplate, 'summary-two-value', totals['two'], 'summary-two-percentage', parseFloat(totals['two'] * 100 / (total - initialTotals['two'])).toFixed(2)));
+		eggsCountRow = eggsCountRow.replace(fiveColumnPlaceholder, fillColumn(inactiveColumnTemplate, 'summary-five-value', totals['five'], 'summary-five-percentage', parseFloat(totals['five'] * 100 / (total - initialTotals['five'])).toFixed(2)));
+		eggsCountRow = eggsCountRow.replace(sevenColumnPlaceholder, fillColumn(inactiveColumnTemplate, 'summary-seven-value', totals['seven'], 'summary-seven-percentage', parseFloat(totals['seven'] * 100 / (total - initialTotals['seven'])).toFixed(2)));
+		eggsCountRow = eggsCountRow.replace(tenColumnPlaceholder, fillColumn(inactiveColumnTemplate, 'summary-ten-value', totals['ten'], 'summary-ten-percentage', parseFloat(totals['ten'] * 100 / (total - initialTotals['ten'])).toFixed(2)));
 		
-		return row;
+		return eggsSummaryRow + eggsCountRow;
 	},
 	
 	fillColumn = function( columnTemplate, inputId, value, percentageId, percentage ) {
@@ -187,9 +232,10 @@ var initialEggData = JSON.stringify({
 	update = function( index, line, type ) {
 		var prevTotal = index > 0 ? eggData.lines[index - 1].total : 0;
 		$('#' + index + '-' + type + '-value').val(line[type]);
-		$('#' + index + '-total').html(line.total);
+		$('#' + index + '-total .total').html(line.total + ' eggs');
 		$('#' + index + '-two-percentage').html(parseFloat(line.two * 100 / (line.total - prevTotal)).toFixed(2) + '%');
 		$('#' + index + '-five-percentage').html(parseFloat(line.five * 100 / (line.total - prevTotal)).toFixed(2) + '%');
+		$('#' + index + '-seven-percentage').html(parseFloat(line.seven * 100 / (line.total - prevTotal)).toFixed(2) + '%');
 		$('#' + index + '-ten-percentage').html(parseFloat(line.ten * 100 / (line.total - prevTotal)).toFixed(2) + '%');
 		
 		updateSummaryRow(index, line, type);
@@ -200,6 +246,7 @@ var initialEggData = JSON.stringify({
 		$('#summary-' + type + '-value').val(totals[type]);
 		$('#summary-two-percentage').html(parseFloat(totals['two'] * 100 / (total - initialTotals['two'])).toFixed(2) + '%');
 		$('#summary-five-percentage').html(parseFloat(totals['five'] * 100 / (total - initialTotals['five'])).toFixed(2) + '%');
+		$('#summary-seven-percentage').html(parseFloat(totals['seven'] * 100 / (total - initialTotals['seven'])).toFixed(2) + '%');
 		$('#summary-ten-percentage').html(parseFloat(totals['ten'] * 100 / (total - initialTotals['ten'])).toFixed(2) + '%');
 	};	
 
@@ -224,12 +271,14 @@ $('#new-line').on('click', function() {
 	var table = $('#table');
 		lastRowIndex = eggData.lines.length - 1,
 		lastRow = $('#' + lastRowIndex),
-		newRow = rowTemplate, 
+		newEggsSummaryRow = eggsSummaryRowTemplate,
+		newEggsCountRow = eggsCountRowTemplate,
 		newRowIndex = lastRowIndex + 1,
 		newLine = {
 			'total' : eggData.lines[lastRowIndex].total,
 			'two' : 0,
 			'five' : 0,
+			'seven': 0,
 			'ten' : 0,
 			'history' : false,
 			'since' : new Date().toISOString().slice(0,10)
@@ -240,16 +289,18 @@ $('#new-line').on('click', function() {
 	eggData.lines[lastRowIndex].history = true;
 	eggData.lines.push(newLine);
 	
-	newRow = newRow.replace(trIdPlaceholder, newRowIndex);
-	newRow = newRow.replace(trClassPlaceholder,  '');
-	newRow = newRow.replace(totalIdPlaceholder, newRowIndex + '-total');
-	newRow = newRow.replace(totalPlaceholder, newLine.total);
-	newRow = newRow.replace(sincePlaceholder, newLine.since);
-	newRow = newRow.replace(twoColumnPlaceholder, createColumn(activeColumnTemplate, 'two', newRowIndex));
-	newRow = newRow.replace(fiveColumnPlaceholder, createColumn(activeColumnTemplate, 'five', newRowIndex));
-	newRow = newRow.replace(tenColumnPlaceholder, createColumn(activeColumnTemplate, 'ten', newRowIndex));	
+	newEggsSummaryRow = newEggsSummaryRow.replace(totalIdPlaceholder, newRowIndex + '-total');
+	newEggsSummaryRow = newEggsSummaryRow.replace(totalPlaceholder, newLine.total + ' eggs');
+	newEggsSummaryRow = newEggsSummaryRow.replace(sincePlaceholder, 'started on ' + newLine.since);
+	newEggsCountRow = newEggsCountRow.replace(trIdPlaceholder, newRowIndex);
+	newEggsCountRow = newEggsCountRow.replace(trClassPlaceholder,  '');
+	newEggsCountRow = newEggsCountRow.replace(twoColumnPlaceholder, createColumn(activeColumnTemplate, 'two', newRowIndex));
+	newEggsCountRow = newEggsCountRow.replace(fiveColumnPlaceholder, createColumn(activeColumnTemplate, 'five', newRowIndex));
+	newEggsCountRow = newEggsCountRow.replace(sevenColumnPlaceholder, createColumn(activeColumnTemplate, 'seven', newRowIndex));
+	newEggsCountRow = newEggsCountRow.replace(tenColumnPlaceholder, createColumn(activeColumnTemplate, 'ten', newRowIndex));	
 	
-	lastRow.after(newRow);
+	lastRow.after(newEggsCountRow);
+	lastRow.after(newEggsSummaryRow);
 	
 	$('.plus').on('click', increment); 
 	$('.minus').on('click', decrement);
